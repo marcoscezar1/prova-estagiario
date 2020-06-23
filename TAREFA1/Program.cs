b015de22cbd5d94850c2e9bd177c5b64bf56b167
marcoscezar1 <cezar.mp23@gmail.com>
2020-06-23 01:46:21 -0300

    Iniciando primeira tarefa

---
+++ TAREFA1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TAREFA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
