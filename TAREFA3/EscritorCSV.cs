440b1f12994650350e3ede22292a3d88f40081ec
marcoscezar1 <cezar.mp23@gmail.com>
2020-06-23 03:01:32 -0300

    Criando classes EscritorCSV e LeitorCSV

---
+++ TAREFA3/EscritorCSV.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using System.Collections.Generic;
+using Modelos;
+using System;
+
+namespace TAREFA2
+{
+    class EscritorCSV
+    {
+        private string caminhoArquivo;
+        public EscritorCSV(string caminhoArquivo)
+        {
+            this.caminhoArquivo = caminhoArquivo;
+        }
+
+
+        //Função que escreve cada item de uma lista em uma linha no padrão csv
+        //Ela tem como retorno o status da operação. Com isso é possível saber se ela foi bem sucessedida
+        public int EscreverTodasAsLinhas(string cabecalho, List<Endereco> listaDeEnderecos)
+        {
+            if (!listaDeEnderecos.Equals(null))
+            {
+                using (var escritor = new StreamWriter(caminhoArquivo))
+                {
+                    escritor.WriteLine(cabecalho);
+                    foreach (Endereco endereco in listaDeEnderecos)
+                    {
+                        string linha = $"{endereco.Cep}; {endereco.Logradouro}; {endereco.Complemento}; {endereco.Bairro}; {endereco.Localidade};"+ 
+                        $"{endereco.Uf}; {endereco.Unidade}; {endereco.Ibge}; {endereco.Bairro}; {endereco.Localidade}; {endereco.Uf}; {endereco.Unidade}; {endereco.Gia}" ;
+                        escritor.WriteLine(linha);
+                    }
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ TAREFA3/LeitorCSV.cs
@@ -0,0 +1,53 @@
+using System.IO;
+using System.Collections.Generic;
+using System;
+using Modelos;
+
+namespace TAREFA2
+{
+    class LeitorCSV
+    {
+        //Atributo que guarda o caminho do arquivo a ser lido
+        private string caminhoArquivo;
+        private string cabecalho;
+        public LeitorCSV(string caminhoArquivo)
+        {
+            this.caminhoArquivo = caminhoArquivo;
+        }
+        
+        public string getCabecalho(){
+            return this.cabecalho;
+        }
+
+        /*
+        Método que retorna todos os ceps do arquivo CSV
+        O método testa se o arquivo existe e caso o contrário, ele retorna uma mensagem de erro e a aplicação termina
+        */
+        public List<string> lerTodasAsLinhas()
+        {
+            try
+            {
+                using (var leitor = new StreamReader(this.caminhoArquivo))
+                {
+                    List<string> listaDeCeps = new List<string>();
+                    //Pulando o cabeçalho
+                    this.cabecalho = leitor.ReadLine();
+                    while (!leitor.EndOfStream)
+                    {
+                        var linha = leitor.ReadLine();
+                        var valores = linha.Split(";");
+                        string cep = valores[0];
+                        listaDeCeps.Add(cep);
+                    }
+                    return listaDeCeps;
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Arquivo não encontrado");
+                Environment.Exit(0);
+                return null;
+            }
+        }
+    }
+}
