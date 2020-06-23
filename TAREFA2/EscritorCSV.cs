1411f9684b2496fad04df66e4aa2941b8173f6f5
marcoscezar1 <cezar.mp23@gmail.com>
2020-06-23 02:24:49 -0300

    Adicionando classe EscritorCSV e LeitorCSV

---
+++ TAREFA2/EscritorCSV.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using System.Collections.Generic;
+using Modelos;
+using System;
+
+namespace TAREFA1
+{
+    class EscritorCSV
+    {
+        private string caminhoArquivo;
+        public EscritorCSV(string caminhoArquivo)
+        {
+            this.caminhoArquivo = caminhoArquivo;
+        }
+
+
+        //Função que escreve cada item de uma lista em uma linha no padrão csv
+        //Ela tem como retorno o status da operação. Com isso é possível saber se ela foi bem sucessedida
+        public int EscreverTodasAsLinhas(string cabecalho, List<Cidade> listaDeCidades)
+        {
+            if (!listaDeCidades.Equals(null))
+            {
+                using (var escritor = new StreamWriter(caminhoArquivo))
+                {
+                    escritor.WriteLine(cabecalho);
+                    foreach (Cidade cidade in listaDeCidades)
+                    {
+                        string linha = $"{cidade.getCidade()}; {cidade.getPopulacao()}";
+                        escritor.WriteLine(linha);
+                        Console.WriteLine(linha);
+                    }
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ TAREFA2/LeitorCSV.cs
@@ -0,0 +1,53 @@
+using System.IO;
+using System.Collections.Generic;
+using System;
+using Modelos;
+
+namespace TAREFA1
+{
+    class LeitorCSV
+    {
+        //Atributo que guarda o caminho do arquivo a ser lido
+        private string caminhoArquivo;
+        private string cabecalho;
+        public LeitorCSV(string caminhoArquivo)
+        {
+            this.caminhoArquivo = caminhoArquivo;
+        }
+        
+        public string getCabecalho(){
+            return this.cabecalho;
+        }
+
+        /*
+        Método que retorna todas as linhas do arquvo csv em uma lista de objetos do tipo Cidade
+        O método testa se o arquivo existe e caso o contrário, ele retorna uma mensagem de erro e a aplicação termina
+        */
+        public List<Cidade> lerTodasAsLinhas()
+        {
+            try
+            {
+                using (var leitor = new StreamReader(this.caminhoArquivo))
+                {
+                    List<Cidade> ListaDeCidades = new List<Cidade>();
+                    //Pulando o cabeçalho
+                    this.cabecalho = leitor.ReadLine();
+                    while (!leitor.EndOfStream)
+                    {
+                        var linha = leitor.ReadLine();
+                        var valores = linha.Split(";");
+                        Cidade novaCidade = new Cidade(valores[0], Int32.Parse(valores[1]));
+                        ListaDeCidades.Add(novaCidade);
+                    }
+                    return ListaDeCidades;
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Arquivo não encontrado");
+                Environment.Exit(0);
+                return null;
+            }
+        }
+    }
+}
