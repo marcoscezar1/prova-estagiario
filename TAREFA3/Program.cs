e0b758423522c2f4d6b79847ab7fe367ac786ef6
marcoscezar1 <cezar.mp23@gmail.com>
2020-06-23 02:47:04 -0300

    Iniciando terceira tarefa

---
+++ TAREFA3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TAREFA3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
